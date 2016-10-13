7c57acc8fbacaafdeeafcd55b679f586625f08e6
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 10:51:36 -0400

    Create U03_S02_P04.js

---
+++ U03_S02_P04.js
@@ -0,0 +1,77 @@
+/**
+ * @link https://studio.code.org/s/cspunit3/stage/2/puzzle/4
+ * @Unit  3
+ * @Stage 2
+ * @Puzzle 4 of 7
+ * Do This:
+ * Draw a 3x3 grid to the front and right of the turtle as efficiently as possible.
+ * The turtle should end at its original location, facing its original direction.
+ * HINT: This problem presents some subtle challenges. 
+ * Work with your partner to discuss possible solutions.
+ * Try more than one path. 
+ * Testing your ideas on paper might be a faster way to brainstorm.
+*/
+
+
+turnLeft();
+turnLeft();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+penUp();
+penUp();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+moveForward();
+turnLeft();
+penDown();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+penUp();
+moveForward();
+turnLeft();
+penDown();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+turnLeft();
+penUp();
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+turnLeft();
+turnLeft();
