6fb6dc5702a29dac1891de110b47f4a68c2becae
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 13:20:39 -0400

    Create U03_S14_P24.js

---
+++ U03_S14_P00/U03_S14_P24.js
@@ -0,0 +1,39 @@
+/**
+ * @link {https://studio.code.org/s/cspunit3/stage/14/puzzle/24}
+ * @name U03_S14_P24.js
+ * 
+ * Wrapping Up Variables - This is Not Math Class
+ * Hopefully you now understand the gist of working with variables. 
+ * The major thing to remember is that every time you see an = sign it means that you are instructing the computer to set a variable to a new value.
+ * The example to the right is something that definitely wouldn't make sense in math class, 
+ * but you should be able to reason about it as a piece of code. 
+ * Because it's code, you can also insert statements to display the value of variables as they change. 
+ * You don't have to guess!
+ * Do This:
+ * 
+ * Make a prediction.
+ * As usual you should first look at the code given to you and reason about what it does. 
+ * You should try to predict what the value of num will be after all lines have executed.
+ * Insert 3 console.log statements.
+ * Insert console.log statements into the existing code to display each value of num immediately after it's changed. (The animation below shows how to get started.)
+ * 
+ * Was your prediction right?
+ * After you figure out the answer, if your prediction was off, try to understand why. Where was your misunderstanding?
+ */
+ 
+
+var num = 0;      // set num = 0 
+console.log ("num="+num);
+num = num + 1;    // Add 1+0 = 1
+console.log ("num="+num);
+num = num + 1;    // 1+1 =2
+console.log ("num="+num);
+num = num + 1;    // 2+1 =3
+console.log ("num="+num);
+
+/**
+num=0
+num=1
+num=2
+num=3
+*/
