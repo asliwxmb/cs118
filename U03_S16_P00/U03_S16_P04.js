afb1e1985f2f7ca84fcbbc89c898118073a71e22
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 18:59:48 -0400

    Create U03_S16_P04.js

---
+++ U03_S16_P00/U03_S16_P04.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * https://studio.code.org/s/cspunit3/stage/16/puzzle/4
+ * U03_S16_P04.js
+ */
+
+console.log("Reminders");
+console.log("1. Finish project for Monday");
+console.log("2. Practice script for presentation");
+console.log("3. Tryouts after school");
