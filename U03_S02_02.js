de9702f1d632897a61105223f785c3e786a71605
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 10:30:01 -0400

    Create U03_S02_02.js

---
+++ U03_S02_02.js
@@ -0,0 +1,10 @@
+// https://studio.code.org/s/cspunit3/stage/2/puzzle/2
+penDown();
+moveForward();
+turnLeft();
+moveForward();
+turnLeft();
+moveForward();
+turnLeft();
+moveForward();
+turnLeft();
