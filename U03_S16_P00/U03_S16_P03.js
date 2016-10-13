faca42f3c7cb0cec0fb17b2775cfe849c8303415
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 18:50:22 -0400

    Create U03_S16_P03.js

---
+++ U03_S16_P00/U03_S16_P03.js
@@ -0,0 +1,23 @@
+
+/** 
+ * Change these strings from the empty string to the kind of word indicated in each variable name
+ * https://studio.code.org/s/cspunit3/stage/16/puzzle/3
+ * @name U06_S19_P03.js
+ */
+ 
+var pluralNoun = "dogs";
+var verb = "run";
+var adjective1 = "scary";
+var adjective2 = "green";
+
+console.log("How to take care of your pet ostrich");
+console.log("1. Make sure your ostrich eats plenty of " + pluralNoun);
+console.log("2. Your ostrich will need to " + verb + " at least a couple of times a day");
+console.log("3. A healthy diet will make sure your ostrich's feathers are " + adjective1 + " and " + adjective2);
+
+/**
+ * How to take care of your pet ostrich
+1. Make sure your ostrich eats plenty of dogs
+2. Your ostrich will need to run at least a couple of times a day
+3. A healthy diet will make sure your ostrich's feathers are scary and green
+*/
