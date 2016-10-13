367478c7bf36e8e5bf13edba86cef1ebc40ad12d
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 13:00:55 -0400

    Create U03_S14_P19.js

---
+++ U03_S14_P00/U03_S14_P19.js
@@ -0,0 +1,20 @@
+/**
+ * Moving Memory - Challenge 2
+ * Try another one!!
+ * Click through to see a new problem provided in the starter code.
+ * A reminder of the rules and example are below if you need to sneak a peek.
+ * Reminder of the rules (click to expand)
+ * Try it yourself...
+ */
+
+
+var x = 7;
+var y = 3;
+// your code here
+y = x -y;
+
+console.log("The value of y is: " + y + " (y should be 4)");
+
+/**
+ * The value of y is: 4 (y should be 4)
+ */
