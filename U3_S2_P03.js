891df23341c75a3957265c541b9c0fc31180d029
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 10:40:28 -0400

    Create U3_S2_P03.js

---
+++ U3_S2_P03.js
@@ -0,0 +1,28 @@
+/**
+ * @link https://studio.code.org/s/cspunit3/stage/2/puzzle/3
+ * Puzzle 3 of 7
+ * Do This:
+ * Draw a 1 x 1 square to the front and right of the turtle as efficiently as possible. 
+ * Notice that there is no command to turn right! 
+ * You need to figure out how to draw this by only turning left and moving forward. 
+ * The program should stop with turtle in its original position, 
+ * facing its original direction.
+ * When you're done click the Finish button to move onto the next problem.
+*/
+
+moveForward();
+turnLeft();
+turnLeft();
+turnLeft();
+moveForward();
+turnLeft();
+turnLeft();
+turnLeft();
+moveForward();
+turnLeft();
+turnLeft();
+turnLeft();
+moveForward();
+turnLeft();
+turnLeft();
+turnLeft();
