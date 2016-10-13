911b7d24e5c805189f4cfca10600a16d4ed9d561
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 10:36:09 -0400

    Update U03_S02_P02.js

--- U03_S02_P02.js
+++ U03_S02_P02.js
@@ -1,4 +1,16 @@
-// https://studio.code.org/s/cspunit3/stage/2/puzzle/2
+ /** @link https://studio.code.org/s/cspunit3/stage/2/puzzle/2
+/**
+ * Given only 4 turtle commands, write code in App Lab to complete the drawings in each of the challenges below. 
+ * Your goal is to write code that solves the problem as efficiently as possible using only the commands provided.
+ * What does “efficient” mean? 
+ * In computer science it can actually mean several different things depending on what you’re talking about. 
+ * Work on these challenges, try to solve them in a way that you think is best and we’ll ask about efficiency at the end.
+ * 
+ * Do This:
+ * Draw a 1 x 1 square to the front and left of the turtle as efficiently as possible. 
+ * The program should stop with turtle in its original position, facing its original direction.
+ * Once you think you have a solution, click the Finish button - it will appear once you run the program.
+*/
 penDown();
 moveForward();
 turnLeft();
