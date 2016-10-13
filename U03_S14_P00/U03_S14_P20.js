07ab391f202397c35fe95ed59b25c2fbbda8623e
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 13:06:14 -0400

    Create U03_S14_P20.js

---
+++ U03_S14_P00/U03_S14_P20.js
@@ -0,0 +1,18 @@
+/**
+ * @link https://studio.code.org/s/cspunit3/stage/14/puzzle/20
+ * Moving Memory - Challenge 3
+ * Try another one. Here is the starting code:
+ 
+ var x = 2;
+ var y = 3;
+// your code here
+
+console.log("The value of x is: " + x + " (x should be 7)");
+*/
+
+var x = 2;
+var y = 3;
+// your code here
+x = (y * y) -x;
+
+console.log("The value of x is: " + x + " (x should be 7)");
