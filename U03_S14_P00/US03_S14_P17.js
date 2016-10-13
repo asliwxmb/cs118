7a9f4621d8c2d554e96f828b87bc044dcdb35e06
asliwxmb <a_sliwkowski@post.massbay.edu>
2016-10-13 12:53:35 -0400

    Create US03_S14_P17.js

---
+++ U03_S14_P00/US03_S14_P17.js
@@ -0,0 +1,18 @@
+/** https://studio.code.org/s/cspunit3/stage/14/puzzle/17
+
+
+var someNum = 7;  // Sets the variable someNum = 7
+
+someNum = someNum + 1;  // Adds 1 to someNum which currently is 7: 7 + 1 =8
+console.log ("The value of someNum is expected to be : 8");
+console.log("The value of someNum is now: " + someNum);
+
+someNum = someNum + 1;  // 8+1 = 9
+console.log("The value of someNum is now: " + someNum);
+
+someNum = someNum + 1; // 9+1 = 10
+console.log("The value of someNum is now: " + someNum);
+
+someNum = someNum * someNum; // 10 * 10 = 100
+console.log("The value of someNum is now: " + someNum);
+
